ffdce837232f9bce9144d905d76203c1780131c1
Alexey <alkvnk@gmail.com>
2019-09-22 15:32:45 +0300

    First proto is created

--- proto.js
+++ proto.js
@@ -1,12 +1,4 @@
-let cat = {
-    length: 0.5,
-    age : 10,
-    eat : function(){
-    }
-    
-};
-
-class Cat {
+class Cat_constr {
     constructor(length, age){
         this.length = length;
         this.age = age;
@@ -15,8 +7,40 @@ class Cat {
 
     }
 }
-var cat1 = new Cat(0.5, 5);
+var cat1 = new Cat_constr(0.5, 5);
 console.log(cat1);
 
-var cat2 = new Cat(0.6, 6);
+var cat2 = new Cat_constr(0.6, 6);
 console.log(cat2);
+///////////////////////////////
+//
+//////////////////////////////
+var animal = {
+    age: 10,
+    eat: function(){
+        return 'ChafChaf'
+
+    }
+};
+
+var cat = {
+    mustache: true,
+    GetVoice: function(){
+        return 'myaMya'
+    }
+
+};
+var dog = {
+    mustache: false,
+    GetVoice: function(){
+        return 'gafGaf'
+    }
+
+};
+
+dog.__proto__ = animal;
+cat.__proto__ = animal;
+
+console.log(cat.GetVoice());
+console.log(cat.age);
+console.log(cat.eat());
